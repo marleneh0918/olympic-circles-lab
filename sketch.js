6017833a98f0844ad504942f9e11389b350b614a
Marlene Hernandez <marleneh50324@students.laalliance.org>
2017-11-19 06:22:24 +0000

    done

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+ createCanvas(800,600)
+}
+
+function draw() {
+  noFill()
+  strokeWeight(6)
+  ellipse(50, 50, 80, 80);
+  ellipse(100, 90, 80, 80);
+  ellipse(150, 50, 80, 80);
+  ellipse(200, 90, 80, 80);
+  ellipse(250, 50, 80, 80);
+}
